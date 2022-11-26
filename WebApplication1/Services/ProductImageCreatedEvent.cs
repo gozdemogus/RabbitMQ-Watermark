1f78cca789b2488cde3b7975d19b86e31d36fe17
gozdemogus <gozdemogus@hotmail.com>
2022-11-26 16:52:34 +0300

    initial commit

---
+++ WebApplication1/Services/ProductImageCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RabbitMQ.Watermark.Services
+{
+	public class ProductImageCreatedEvent
+	{
+		public ProductImageCreatedEvent()
+		{
+		}
+
+		public string ImageName { get; set; }
+	}
+}
+
